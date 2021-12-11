7629517d9ff04043b0c3c5d78628df3b391106eb
Junyi Zhu <junyi.zhu@gmx.de>
2021-12-11 11:30:32 +0100

    update

--- language/bert/bert_code/preprocess/common/length_filter_by_char.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    l = len(line)
-    if int(sys.argv[1]) <= l <= int(sys.argv[2]):
-        sys.stdout.write(line)
--- language/bert/bert_code/preprocess/common/remove_non_utf8_chars.py
+++
@@ -1,5 +0,0 @@
-import io
-import sys
-
-for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8', errors='ignore'):
-    sys.stdout.write(line)
